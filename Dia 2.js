ec343163bae4af03cc3cc9c38747d183b08895f4
Lucas <lucasdossantossilvasantana@gmail.com>
2024-02-24 20:53:02 -0300

    add all

---
+++ Dia 2.js
@@ -0,0 +1,6 @@
+/* DESAFIO 2 DO #7DAYSOFCODE */ 
+
+const nome = prompt ('Qual o seu nome');
+const idade = prompt ('Quantos anos vc tem?');
+const linguagem = prompt  ('Qual linguagem de programação você está aprendendo?');
+console.log ('Olá '+nome+' você tem '+idade+' ano e já está aprendendo '+ linguagem+'!')
